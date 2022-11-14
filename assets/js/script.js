60b0dbdffef5b1021a4d7a3588a52f7bdc23f588
MinuMThomas <minumarytomas@gmail.com>
2022-11-14 16:50:05 +0000

    complete making request

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,21 @@ async function getStatus(e) {
 
     if (response.ok) {
         console.log(data.expiry);
+    } else {
+        throw new Error (data.error);
     }
-
 }
 
+function displayStatus(data){
+        // set the  heading text to API key status  
+        // I set the results variable to the content that  I want in the body using template literals.  
+        // Then using document.getElementById and the  IDs I gave you earlier I set the content.  
+        // And finally, the results modal is shown
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
 
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+}
